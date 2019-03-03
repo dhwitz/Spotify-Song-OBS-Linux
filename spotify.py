c501588f42e9a93a69b72ecb7f3702e22ec90eac
David Horowitz <dhorowit@stevens.edu>
2019-03-03 01:31:30 -0500

    first commit

---
+++ spotify.py
@@ -0,0 +1,18 @@
+import dbus
+import time
+
+# The property Metadata behaves like a python dict
+while(True):
+    session_bus = dbus.SessionBus()
+    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
+                                     "/org/mpris/MediaPlayer2")
+    spotify_properties = dbus.Interface(spotify_bus,
+                                    "org.freedesktop.DBus.Properties")
+    metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    for key, value in metadata.items():
+        print(key, value)
+
+    # To just print the title
+    print(metadata['xesam:title'])
+
+    time.sleep(10)
